45e2dd72af28024e290cb7dd75692def18eec1bc
Saumya <saumyamehta1992sg@gmail.com>
2021-08-10 15:59:31 +0530

    slingshot

--- Chain.js
+++ Chain.js
@@ -1,20 +1,30 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA,pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
+        console.log(pointB)
+        console.log(bodyA)
     }
 
     display(){
+        if (this.chain.bodyA){
+
+      
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+
+        var pointB = this.chain.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    fly(){
+        this.chain.bodyA=null; 
     }
     
 }
